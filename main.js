5df17544fe8c886b56d1ac2852ca184cb31b0866
Victor Schiavo <schiavovictor@outlook.com>
2023-02-12 14:15:30 -0300

    Função para limpar contatos

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 
 const meuForm = document.getElementById('form--principal');
 const upload = document.getElementById('arquivo');
+const limpar = document.querySelector('#limpar');
 const textArea = document.getElementById('msg') ;
-console.log(meuForm, upload, textArea); 
+const contatos = document.querySelector('.container--contatos')
 let CSV;
-let limparCSV;
+// let limparCSV;
 let dados = {}
 
 
@@ -14,16 +15,20 @@ meuForm.addEventListener('submit', (e) => {
     const arquivo = upload.files[0];
     const leitor = new FileReader();
     leitor.onload = (e) => {
-        console.log('content: ', e.target.result)
+        // console.log('content: ', e.target.result)
         CSV = e.target.result.replaceAll('\r', '');
         // limparCSV = CSV.replaceAll('\r', "");
         dados = converterCSV(CSV);
+        popularDados(dados);
     };
     leitor.readAsText(arquivo);
 })
 
+limpar.addEventListener('click', limparContatos);
+
 function converterCSV(string, delimitador = ';') {
-    const cabecalho = string.trim().slice(0, string.indexOf(`\n`)).split(delimitador)
+    // const cabecalho = string.trim().slice(0, string.indexOf(`\n`)).split(delimitador)
+    const cabecalho = ['Nome', 'Zap'];
     console.log(cabecalho)
     const linhas = string.trim().slice(string.indexOf("\n") + 1).split("\n")
     console.log(linhas)
@@ -31,6 +36,9 @@ function converterCSV(string, delimitador = ';') {
         const valores = linha.split(delimitador);
         console.log(valores);
         const elementos = cabecalho.reduce((objeto, cabeçalho, indice) => {
+            // console.log(objeto)
+            // console.log(cabeçalho)
+            // console.log(indice)
             objeto[cabeçalho] = valores[indice];
             return objeto;
         }, {});
@@ -39,9 +47,22 @@ function converterCSV(string, delimitador = ';') {
 return arry
 }
 
+function popularDados(dataSet) {
+    let mensagem = encodeURI(document.querySelector('#msg').value)
+    for (const contato of dataSet) {
+    let string = `<p class="contato--nome">${contato.Nome}</p><a class="contato--zap" href="https://wa.me/${contato.Zap}?text=${mensagem}" target="_blank">https://wa.me/${contato.Zap}</a>`;
+    contatos.insertAdjacentHTML("beforeend", string)
+}
+    
+}
 
-
-
-
-
-
+function limparContatos() {
+    const nomes = document.querySelectorAll('.contato--nome');
+    const zaps =  document.querySelectorAll('.contato--zap');
+    nomes.forEach(el => {
+        el.remove();
+    });
+    zaps.forEach(el => {
+        el.remove()
+    });
+}
