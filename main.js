8e2aba5e0d5ac7bf72bd44bcbdc7ea5c5f9ebce6
seriouslyvictor <schiavovictor@outlook.com>
2023-02-13 10:40:28 -0300

    Limpeza no código

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ return arry
 function popularDados(dataSet) {
     let mensagem = encodeURI(document.querySelector('#msg').value)
     for (const contato of dataSet) {
-    let string = `<p class="contato--nome">${contato.Nome}</p><a class="contato--zap" href="https://whatsa.me/${contato.Zap}?t=${mensagem}" target="_blank">https://whatsa.me/${contato.Zap}</a>`;
+    let string = `<p class="contato--nome">${contato.Nome}</p><a class="contato--zap" href="https://web.whatsapp.com/send/?phone=${contato.Zap}&text=${mensagem}&type=phone_number&app_absent=0" target="_blank">https://wa.me/${contato.Zap}</a>`;
     contatos.insertAdjacentHTML("beforeend", string)
 }
     
